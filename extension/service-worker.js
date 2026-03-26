b2aeb4103ddda2dfb545087fbf6a105ad33be2a1
SMXlol <slyncukmaksim@gmail.com>
2026-03-26 22:40:55 +0700

    Revert "Slim content payload and replace stale scripts"
    This reverts commit 3e44f693cfa8c339bab4b2cdc39f78b38130abfa.

--- extension/service-worker.js
+++ extension/service-worker.js
@@ -78,7 +78,7 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
         sendResponse({
           ok: true,
           settings,
-          bundle: createFilteringBundle(bundle),
+          bundle,
           isTrusted: isTrustedHost(hostname, settings.trustedSites)
         });
         return;
@@ -315,16 +315,6 @@ function normalizeSourceConfig(payload) {
 function setActiveBundle(bundle) {
   activeBundle = bundle;
   activeBundleIndex = createBundleIndex(bundle);
-}
-
-function createFilteringBundle(bundle) {
-  return {
-    version: bundle.version,
-    updatedAt: bundle.updatedAt,
-    metadata: bundle.metadata,
-    dictionary: bundle.dictionary || [],
-    exceptions: bundle.exceptions || []
-  };
 }
 
 function createBundleIndex(bundle) {
